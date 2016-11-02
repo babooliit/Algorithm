d25c080d562d85030f21ebd192bd4339d63747b7
babooliit <hbabool.bharat.cer13@iitbhu.ac.in>
2016-11-03 01:31:27 +0530

    Ugly Number
    Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The
    sequence
    1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
    shows the first 11 ugly numbers. By convention, 1 is included.
    Write a program to find and print the 150’th ugly number.

---
+++ dynamic-programming/UglyNumber.java
@@ -0,0 +1,42 @@
+
+public class UglyNumber {
+	static int getNthUglyNo(int n){
+		int i2 = 0;
+		int i3 = 0;
+		int i5 = 0;
+		int nextUgly = 1;
+		int nextMultipleOf2 = 2;
+		int nextMultipleOf3 = 3;
+		int nextMultipleOf5 = 5;
+		int ugly[] = new int[n];
+		ugly[0] = 1;
+		
+		for(int i=1;i<n;){
+			nextUgly = Math.min(Math.min(nextMultipleOf2, nextMultipleOf3), nextMultipleOf5);
+			ugly[i] = nextUgly;
+			if(nextUgly == nextMultipleOf2){
+				i2++;
+				nextMultipleOf2 = ugly[i2]*2;
+			}
+			if(nextUgly == nextMultipleOf3){
+				i3++;
+				nextMultipleOf3 = ugly[i3]*3;
+			}
+			if(nextUgly == nextMultipleOf5){
+				i5++;
+				nextMultipleOf5 = ugly[i5]*5;
+			}
+			ugly[i++] = nextUgly;
+		}
+		return nextUgly;	
+		
+	}
+
+	public static void main(String[] args) {
+		for(int n = 1;n<151; n++){
+		int no = getNthUglyNo(n);
+		System.out.println(n+"th Ugly Number is "+no);
+		}
+	}
+
+}
